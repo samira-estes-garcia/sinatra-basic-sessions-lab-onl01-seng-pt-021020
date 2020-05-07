b2625ec5dc522d417772b1774fb06ba1620a4997
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-05-07 18:29:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   configure do 
-    enable sessions 
+    enable :sessions 
+    :sessions_secret , "secret"
   end
   
   get '/' do 
